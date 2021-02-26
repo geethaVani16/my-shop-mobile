4127ab31022714db6ed8cf221bb256840b2c68e4
geethaVani16 <geetha53813@gmail.com>
2021-02-26 17:35:28 +0530

    added create and update

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,29 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
-import { View, StyleSheet, Text, ScrollView, TextInput } from "react-native"
+import { View, StyleSheet, Text, ScrollView, TextInput, Platform } from "react-native"
+import { useDispatch, useSelector } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import CustomHeaderButton from '../../components/shop/UI/headerButton'
+import { createProduct, updateProduct } from '../../store/actions/productAction'
+
 
 const EditProductScreen = props => {
-    const 
+
+    const prodId = props.navigation.getParam('productId');
+    const editedProduct = useSelector(state =>
+        state.products.userProducts.find(prod => prod.id === prodId)
+    );
+    const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
+    const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '')
+    const [price, setPrice] = useState(editedProduct ? editedProduct.price : '')
+    const [description, setDescription] = useState(editedProduct ? editedProduct.description : '')
+    const dispatch=useDispatch()
+
+    const submitHandler = useCallback(() => {
+       if(editedProduct) {
+           dispatch(updateProduct(prodId,title,description,imageUrl))
+       } else {
+           console.log(title,description,imageUrl,+price)
+           dispatch(createProduct(title,description,imageUrl,+price))
+       }
+    }, [dispatch,prodId,title,description,imageUrl]);
+
+
+
+    useEffect(() => {
+        props.navigation.setParams({ submit: submitHandler })
+    }, [submitHandler])
     return (
         <ScrollView>
             <View style={styles.form}>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Title</Text>
-                    <TextInput style={styles.input} />
+                    <TextInput
+                        style={styles.input}
+                        value={title}
+                        onChangeText={text => setTitle(text)}
+                    />
                 </View>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>ImageUrl</Text>
-                    <TextInput style={styles.input} />
+                    <TextInput
+                        style={styles.input}
+                        value={imageUrl}
+                        onChangeText={text => setImageUrl(text)}
+                    />
                 </View>
-                <View style={styles.formControl}>
-                    <Text style={styles.label}>Price</Text>
-                    <TextInput style={styles.input} />
-                </View>
+                {editedProduct ? null : (
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>Price</Text>
+                        <TextInput
+                            style={styles.input}
+                            value={price}
+                            onChangeText={text => setPrice(text)}
+                        />
+                    </View>
+                )}
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Description</Text>
-                    <TextInput style={styles.input} />
+                    <TextInput
+                        style={styles.input}
+                        value={description}
+                        onChangeText={text => setDescription(text)}
+                    />
                 </View>
             </View>
         </ScrollView>
@@ -32,16 +77,15 @@ const EditProductScreen = props => {
 
 
 EditProductScreen.navigationOptions = navData => {
+    const submitFn = navData.navigation.getParam('submit')
     return {
         headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
         headerRight: () => {
             return <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                 <Item
                     title='edit'
-                    iconName={Platform.OS === 'android ' ? 'md-create' : 'ios-create'}
-                    onPress={() => {
-                        navData.navigation.navigate('EditProduct')
-                    }}
+                    iconName={Platform.OS === 'android ' ? 'md-checkmark' : 'ios-checkmark'}
+                    onPress={submitFn}
 
                 />
 
@@ -67,7 +111,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 2,
         paddingVertical: 5,
         borderBottomColor: '#ccc',
-        borderBottomWidth: 1
+        borderBottomWidth: 1,
     }
 
 })
--- store/reducers/productReducer.js
+++ store/reducers/productReducer.js
@@ -1,5 +1,6 @@
 import PRODUCTS from '../../data/dummy-data';
-import { DELETE_PRODUCT } from '../actions/productAction';
+import Product from '../../models/product'
+import { CREATE_PRODUCT, DELETE_PRODUCT, UPDATE_PRODUCT } from '../actions/productAction';
 
 
 const intialState = {
@@ -13,12 +14,51 @@ export default (state = intialState, action) => {
             return {
                 ...state,
                 userProducts: state.userProducts.filter(
-                  product => product.id !== action.pid
+                    product => product.id !== action.pid
                 ),
                 availableProducts: state.availableProducts.filter(
                     product => product.id !== action.pid
-                  ),
+                ),
             }
+        case CREATE_PRODUCT:
+            const newProduct = new Product(
+                action.productData.id,
+                'u1',
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                action.productData.price
+            );
+            return {
+                ...state,
+                availableProducts: state.availableProducts.concat(newProduct),//for adding new product
+                userProducts: state.userProducts.concat(newProduct)
+            };
+        case UPDATE_PRODUCT:
+            const productIndex = state.userProducts.findIndex(
+                prod => prod.id === action.pid
+            );
+            const updatedProduct = new Product(
+                action.pid,
+                state.userProducts[productIndex].ownerId,
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                state.userProducts[productIndex].price
+            );
+            const updatedUserProducts = [...state.userProducts];
+            updatedUserProducts[productIndex] = updatedProduct;
+            const availableProductIndex = state.availableProducts.findIndex(
+                prod => prod.id === action.pid
+            );
+            const updatedAvailableProducts = [...state.availableProducts];
+            updatedAvailableProducts[availableProductIndex] = updatedProduct;
+            return {
+                ...state,
+                availableProducts: updatedAvailableProducts,
+                userProducts: updatedUserProducts
+            };
+
     }
 
     return state
